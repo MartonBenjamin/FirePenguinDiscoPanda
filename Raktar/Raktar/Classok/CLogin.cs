24be1e2b5cd4b5e3830d69ffb94ccd177bd3d270
MartonBenjamin <43416014+MartonBenjamin@users.noreply.github.com>
2018-10-24 18:24:28 +0200

    Kiosztás, classok, menü

--- Raktar/Raktar/Classok/CLogin.cs
+++ Raktar/Raktar/Classok/CLogin.cs
@@ -8,58 +8,60 @@ namespace Raktar
 {
     class CLogin
     {
-        using (firepenguinEntities1 db = new firepenguinEntities1())
-            {
+//        using (firepenguinEntities1 db = private new firepenguinEntities1())
+//            {
 
 
-                var user = db.Logins.FirstOrDefault(u => u.felhasznalonev == tblogin.Text);
-    int maxId = db.Logins.Select(p => p.id).Max();
-                //user.felhasznalonev = "admin";
-                //user.jelszo = "admin";
-                //user.id = maxId + 1;
-                //db.Loginadatok.Add(user);
-                //db.SaveChanges();
-                if (user != null)
-                {
+//                var user = db.Logins.FirstOrDefault(u => u.felhasznalonev == tblogin.Text);
+//    int maxId = db.Logins.Select(p => p.id).Max();
+//                //user.felhasznalonev = "admin";
+//                //user.jelszo = "admin";
+//                //user.id = maxId + 1;
+//                //db.Loginadatok.Add(user);
+//                //db.SaveChanges();
+//                if (user != null)
+//                {
                     
-                    if (user.jelszo == pswbox.Password)
-                    {
-                        lblloginfo.Visibility = Visibility.Visible;
-                        lblloginfo.Content = "Sikeres bejelentkezés!";
-                        lblloginfo.Foreground = Brushes.Green;
-                        Bejelentkezes();
-}
-                    else
-                    {
-                        lblloginfo.Visibility = Visibility.Visible;
+//                    if (user.jelszo == pswbox.Password)
+//                    {
+//                        lblloginfo.Visibility = Visibility.Visible;
+//                        lblloginfo.Content = "Sikeres bejelentkezés!";
+//                        lblloginfo.Foreground = Brushes.Green;
+//                        Bejelentkezes();
+//}
+//                    else
+//                    {
+//                        lblloginfo.Visibility = Visibility.Visible;
 
-                        lblloginfo.Content = "Hibás jelszó!";
-                        lblloginfo.Foreground = Brushes.Red;
-
-
-                    }
-
-                }
-                else
-                {
-                    lblloginfo.Visibility = Visibility.Visible;
-
-                    lblloginfo.Content = "Hibás felhasználónév!";
-                    lblloginfo.Foreground = Brushes.Red;
+//                        lblloginfo.Content = "Hibás jelszó!";
+//                        lblloginfo.Foreground = Brushes.Red;
 
 
-                }
+//                    }
 
-            }
+//                }
+//                else
+//                {
+//                    lblloginfo.Visibility = Visibility.Visible;
+
+//                    lblloginfo.Content = "Hibás felhasználónév!";
+//                    lblloginfo.Foreground = Brushes.Red;
+
+
+//                }
+
+//            }
             
-        }
-        private void Bejelentkezes()
-{
-    Window1 win1 = new Window1();
-    win1.Show();
-    this.Close();
-    //Fomenu!!
-}
+//        }
+//        private void Bejelentkezes()
+//{
+//    Window1 win1 = new Window1();
+//    win1.Show();
+//    this.Close();
+//    //Fomenu!!
+//}
+////Login class (xml fájlba logokat készít) log típusu enumok
+
 
     }
 }
---
+++ Raktar/Raktar/Pagek/Dolgozok.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Raktar
+{
+    /// <summary>
+    /// Interaction logic for Dolgozok.xaml
+    /// </summary>
+    public partial class Dolgozok : Page
+    {
+        public Dolgozok()
+        {
+            InitializeComponent();
+        }
+    }
+}
