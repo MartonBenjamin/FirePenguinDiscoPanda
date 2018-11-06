fa57b00f36cb5928a87ba30c52a79e9bd5685407
MartonPeterMarton <martonpeter95@gmail.com>
2018-11-06 15:02:31 +0100

    Raktarkezeles alap
    Még nincs kész teljesen.

--- Raktar/Raktar/Classok/CRaktarakKezeles.cs
+++ Raktar/Raktar/Classok/CRaktarakKezeles.cs
@@ -9,5 +9,39 @@ namespace Raktar
     class CRaktarakKezeles
     {
         //Raktár adatainak kilistázása, raktár eltávolítása, hozzáadása
+
+        static List<CRaktarakKezeles> termek = new List<CRaktarakKezeles>();
+
+        public int Id
+        {
+            get; set;
+        }
+        public string Raktarnev
+        {
+            get; set;
+        }
+        public int Iranyitoszam
+        {
+            get; set;
+        }
+        public string Raktarcim
+        {
+            get; set;
+        }
+        public int Kapacitas
+        {
+            get; set;
+        }
+
+        public bool Ures
+        { get; set; }
+        public override string ToString()
+        {
+            return Id + " " + Raktarnev + " " + Iranyitoszam + " " + Raktarcim + " " + Kapacitas + " " + Ures + " ";
+        }
+        public static void RaktarHozzaAd()
+        {
+
+        }
     }
 }
