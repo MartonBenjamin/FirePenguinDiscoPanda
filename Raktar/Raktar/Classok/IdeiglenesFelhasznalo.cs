de4d36c39f94af858162287baf25c61938665517
MartonPeterMarton <martonpeter95@gmail.com>
2018-11-20 20:34:41 +0100

    Látogató
    Ideiglenes nem ott dolgozó (diák/egyetemista/látogató) regisztrálása a rendszerbe.

---
+++ Raktar/Raktar/Classok/IdeiglenesFelhasznalo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktar.Classok
+{
+    class IdeiglenesFelhasznalo
+    {
+        
+        private string vezeteknev, keresztnev,  taj, irsz, anyjaneve;
+        DateTime szulido;
+
+
+
+        public string Vezeteknev
+        {
+            get { return vezeteknev; }
+            set { vezeteknev = value; }
+        }
+        public string Keresztnev
+        {
+            get { return keresztnev; }
+            set { keresztnev = value; }
+        }
+        public DateTime Szulido
+        {
+            get { return szulido; }
+            set { szulido = value; }
+        }
+        
+        public string Taj
+        {
+            get { return taj; }
+            set { taj = value; }
+        }
+        public string Irsz
+        {
+            get { return irsz; }
+            set { irsz = value; }
+        }
+        public string Anyjaneve
+        {
+            get { return anyjaneve; }
+            set { anyjaneve = value; }
+        }
+
+        
+
+        
+
+
+    }
+  
+    
+}
+
